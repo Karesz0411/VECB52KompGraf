24f57b89ed1edacb25fa39971912522021a0f36e
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2024-04-04 12:40:56 +0200

    Last week
    I forgot to push last week. 03.28

---
+++ VECB52_0328/GUIVECB52/src/main/java/hu/the/vecb52/GUIVECB52.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hu.the.vecb52;
+
+/**
+ *
+ * @author Hallgató
+ */
+public class GUIVECB52 {
+
+    public static void main(String[] args) {
+        HelloWorld.main(args);
+    }
+}
